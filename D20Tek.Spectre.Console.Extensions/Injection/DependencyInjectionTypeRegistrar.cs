770307741610eb5469c716f36e2e3451eba2fe8b
Pedro Silva <pedrosi@microsoft.com>
2022-05-14 12:18:54 -0700

    Created Spectre console extensions project. Added type registrar and resolver for the Microsoft.Extensions.DependencyInjection framework.

---
+++ D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeRegistrar.cs
@@ -0,0 +1,48 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    public sealed class DependencyInjectionTypeRegistrar : ITypeRegistrar
+    {
+        private readonly IServiceCollection _builder;
+
+        public DependencyInjectionTypeRegistrar(IServiceCollection builder)
+        {
+            _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+        }
+
+        public ITypeResolver Build()
+        {
+            var provider = _builder.BuildServiceProvider();
+            return new DependencyInjectionTypeResolver(provider);
+        }
+
+        public void Register(Type service, Type implementation)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+
+            _builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterInstance(Type service, object implementation)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+
+            _builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterLazy(Type service, Func<object> func)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(func, nameof(func));
+
+            _builder.AddSingleton(service, (provider) => func());
+        }
+    }
+}
---
+++ D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeResolver.cs
@@ -0,0 +1,37 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    public class DependencyInjectionTypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly IServiceProvider _provider;
+
+        public DependencyInjectionTypeResolver(IServiceProvider provider)
+        {
+            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+        }
+
+        public object? Resolve(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            return _provider.GetService(type);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (_provider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
