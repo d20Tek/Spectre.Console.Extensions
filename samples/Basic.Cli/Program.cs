1ff00c18197f65a59df6dd80860f95d26fd48de0
Pedro Silva <pedrosi@microsoft.com>
2022-05-14 16:29:11 -0700

    Added a basic CommandApp that uses DI but simple service and command configuration

---
+++ samples/Basic.Cli/Program.cs
@@ -0,0 +1,41 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Samples.Common.Commands;
+using D20Tek.Samples.Common.Services;
+using D20Tek.Spectre.Console.Extensions.Injection;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace D20Tek.CountryService.Cli
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            // Create the DI container.
+            var services = new ServiceCollection();
+
+            // configure services here...
+            services.AddSingleton<IDisplayWriter, ConsoleDisplayWriter>();
+            var registrar = new DependencyInjectionTypeRegistrar(services);
+
+            // Create the CommandApp with specified command type and type registrar.
+            var app = new CommandApp<DefaultCommand>(registrar);
+
+            // Configure any commands in the application.
+            app.Configure(config =>
+            {
+                config.CaseSensitivity(CaseSensitivity.None);
+                config.SetApplicationName("Basic.Cli");
+                config.ValidateExamples();
+
+                config.AddCommand<DefaultCommand>("default")
+                    .WithDescription("Default command that displays some text.")
+                    .WithExample(new[] { "default", "--verbose", "high" });
+            });
+
+            return await app.RunAsync(args);
+        }
+    }
+}
