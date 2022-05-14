623a23f4259791401c14f30fa17c404663176a97
Pedro Silva <pedrosi@microsoft.com>
2022-05-14 12:42:55 -0700

    Added unit test project to cover dependency injection classes. Added xml documentation to library public classes.

--- D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeRegistrar.cs
+++ D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeRegistrar.cs
@@ -6,21 +6,40 @@ using Spectre.Console.Cli;
 
 namespace D20Tek.Spectre.Console.Extensions.Injection
 {
+    /// <summary>
+    /// Type registry for Spectre.Console that uses the Microsoft.Extensions.DependencyInjection
+    /// framework to register types with the DI engine.
+    /// </summary>
     public sealed class DependencyInjectionTypeRegistrar : ITypeRegistrar
     {
         private readonly IServiceCollection _builder;
 
+        /// <summary>
+        /// Constructor that takes a service collection instance.
+        /// </summary>
+        /// <param name="builder">Service collection builder to use for registering types.</param>
         public DependencyInjectionTypeRegistrar(IServiceCollection builder)
         {
-            _builder = builder ?? throw new ArgumentNullException(nameof(builder));
+            ArgumentNullException.ThrowIfNull(builder, nameof(builder));
+            _builder = builder;
         }
 
+        /// <summary>
+        /// Builds the type resolver representing the registrations
+        /// specified in the current instance.
+        /// </summary>
+        /// <returns>A type resolver.</returns>
         public ITypeResolver Build()
         {
             var provider = _builder.BuildServiceProvider();
             return new DependencyInjectionTypeResolver(provider);
         }
 
+        /// <summary>
+        /// Registers the specified service.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="implementation">The implementation.</param>
         public void Register(Type service, Type implementation)
         {
             ArgumentNullException.ThrowIfNull(service, nameof(service));
@@ -29,6 +48,11 @@ namespace D20Tek.Spectre.Console.Extensions.Injection
             _builder.AddSingleton(service, implementation);
         }
 
+        /// <summary>
+        /// Registers the specified instance.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="implementation">The implementation.</param>
         public void RegisterInstance(Type service, object implementation)
         {
             ArgumentNullException.ThrowIfNull(service, nameof(service));
@@ -37,12 +61,17 @@ namespace D20Tek.Spectre.Console.Extensions.Injection
             _builder.AddSingleton(service, implementation);
         }
 
-        public void RegisterLazy(Type service, Func<object> func)
+        /// <summary>
+        /// Registers the specified instance lazily.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="factoryMethod">The factory that creates the implementation.</param>
+        public void RegisterLazy(Type service, Func<object> factoryMethod)
         {
             ArgumentNullException.ThrowIfNull(service, nameof(service));
-            ArgumentNullException.ThrowIfNull(func, nameof(func));
+            ArgumentNullException.ThrowIfNull(factoryMethod, nameof(factoryMethod));
 
-            _builder.AddSingleton(service, (provider) => func());
+            _builder.AddSingleton(service, (provider) => factoryMethod());
         }
     }
 }
--- D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeResolver.cs
+++ D20Tek.Spectre.Console.Extensions/Injection/DependencyInjectionTypeResolver.cs
@@ -5,15 +5,30 @@ using Spectre.Console.Cli;
 
 namespace D20Tek.Spectre.Console.Extensions.Injection
 {
-    public class DependencyInjectionTypeResolver : ITypeResolver, IDisposable
+    /// <summary>
+    /// Type resolver for Spectre.Console that uses the Microsoft.Extensions.DependencyInjection
+    /// framework.
+    /// </summary>
+    public sealed class DependencyInjectionTypeResolver : ITypeResolver, IDisposable
     {
         private readonly IServiceProvider _provider;
 
+        /// <summary>
+        /// Constructor that takes a service provider instance.
+        /// </summary>
+        /// <param name="provider">Provider to use in type resolution.</param>
         public DependencyInjectionTypeResolver(IServiceProvider provider)
         {
-            _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+            ArgumentNullException.ThrowIfNull(provider, nameof(provider));
+            _provider = provider;
         }
 
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="type">The type to resolve.</param>
+        /// <returns>An instance of the specified type, or null if no registration for that
+        /// type exists.</returns>
         public object? Resolve(Type? type)
         {
             if (type == null)
@@ -24,6 +39,7 @@ namespace D20Tek.Spectre.Console.Extensions.Injection
             return _provider.GetService(type);
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             GC.SuppressFinalize(this);
