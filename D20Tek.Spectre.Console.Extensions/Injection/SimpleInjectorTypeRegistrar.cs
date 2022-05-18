49b99f5414127921f12a4e68b54e5a9e13139ad0
Pedro Silva <pedrosi@microsoft.com>
2022-05-17 19:11:43 -0700

    Implemented SimpleInjector type registrar and resolver.

---
+++ D20Tek.Spectre.Console.Extensions/Injection/SimpleInjectorTypeRegistrar.cs
@@ -0,0 +1,76 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using SimpleInjector;
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    /// <summary>
+    /// Type registry for Spectre.Console that uses the SimpleInjector
+    /// framework to register types with the DI engine.
+    /// </summary>
+    public sealed class SimpleInjectorTypeRegistrar : ITypeRegistrar
+    {
+        private readonly Container _container;
+
+        /// <summary>
+        /// Constructor that takes a standard kernel instance.
+        /// </summary>
+        /// <param name="container">Service builder to use for registering types.</param>
+        public SimpleInjectorTypeRegistrar(Container container)
+        {
+            ArgumentNullException.ThrowIfNull(container, nameof(container));
+            this._container = container;
+        }
+
+        /// <summary>
+        /// Builds the type resolver representing the registrations
+        /// specified in the current instance.
+        /// </summary>
+        /// <returns>A type resolver.</returns>
+        public ITypeResolver Build()
+        {
+            return new SimpleInjectorTypeResolver(this._container);
+        }
+
+        /// <summary>
+        /// Registers the specified service.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="implementation">The implementation type.</param>
+        public void Register(Type service, Type implementation)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+
+            this._container.Register(service, implementation);
+        }
+
+        /// <summary>
+        /// Registers the specified instance.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="implementation">The implementation.</param>
+        public void RegisterInstance(Type service, object implementation)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+
+            this._container.RegisterInstance(service, implementation);
+        }
+
+        /// <summary>
+        /// Registers the specified instance lazily.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        /// <param name="factoryMethod">The factory that creates the implementation.</param>
+        public void RegisterLazy(Type service, Func<object> factoryMethod)
+        {
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+            ArgumentNullException.ThrowIfNull(factoryMethod, nameof(factoryMethod));
+
+            this._container.RegisterInstance(service, () => factoryMethod());
+        }
+    }
+}
---
+++ D20Tek.Spectre.Console.Extensions/Injection/SimpleInjectorTypeResolver.cs
@@ -0,0 +1,62 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using SimpleInjector;
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    /// <summary>
+    /// Type resolver for Spectre.Console that uses the SimpleInjector framework.
+    /// </summary>
+    public sealed class SimpleInjectorTypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly Container _container;
+
+        /// <summary>
+        /// Constructor that takes a container for SimpleInjector.
+        /// </summary>
+        /// <param name="container">Container to use in type resolution.</param>
+        public SimpleInjectorTypeResolver(Container container)
+        {
+            ArgumentNullException.ThrowIfNull(container, nameof(container));
+            this._container = container;
+        }
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="type">The type to resolve.</param>
+        /// <returns>An instance of the specified type, or null if no registration for that
+        /// type exists.</returns>
+        public object? Resolve(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                return this._container.GetInstance(type);
+            }
+            catch (ActivationException)
+            {
+                // if type isn't registered, SimpleInjector throws an exception,
+                // so we manage that here and return null in that case.
+                return null;
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (this._container is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
