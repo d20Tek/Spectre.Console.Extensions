1cc9011f2647d7b2a9210069d5f6f7547e7ed8ae
Pedro Silva <pedrosi@microsoft.com>
2022-05-21 15:16:25 -0700

    Added Autofac DI type registrar and resolver with unit tests.

---
+++ D20Tek.Spectre.Console.Extensions/Injection/AutofacTypeResolver.cs
@@ -0,0 +1,53 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Autofac;
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    /// <summary>
+    /// Type resolver for Spectre.Console that uses the Autofac DI framework.
+    /// </summary>
+    public sealed class AutofacTypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly ILifetimeScope _scope;
+
+        /// <summary>
+        /// Constructor that takes a service provider instance.
+        /// </summary>
+        /// <param name="scope">Scope provider to use in type resolution.</param>
+        public AutofacTypeResolver(ILifetimeScope scope)
+        {
+            ArgumentNullException.ThrowIfNull(scope, nameof(scope));
+            _scope = scope;
+        }
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="type">The type to resolve.</param>
+        /// <returns>An instance of the specified type, or null if no registration for that
+        /// type exists.</returns>
+        public object? Resolve(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            return _scope.IsRegistered(type) ? _scope.Resolve(type) : null;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (_scope is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
