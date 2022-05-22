66bb609c0376a5a9d972ab1803a791c649ec024e
Pedro Silva <pedrosi@microsoft.com>
2022-05-22 15:27:33 -0700

    Added sample project for LightInject integration.

---
+++ samples/LightInject.Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Samples.Common.Commands;
+using D20Tek.Spectre.Console.Extensions;
+
+namespace LightInject.Cli
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await new CommandAppBuilder()
+                             .WithLightInjectContainer()
+                             .WithStartup<Startup>()
+                             .WithDefaultCommand<DefaultCommand>()
+                             .Build()
+                             .RunAsync(args);
+        }
+    }
+}
---
+++ samples/LightInject.Cli/Startup.cs
@@ -0,0 +1,32 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Samples.Common.Commands;
+using D20Tek.Samples.Common.Services;
+using D20Tek.Spectre.Console.Extensions;
+using Spectre.Console.Cli;
+
+namespace LightInject.Cli
+{
+    internal class Startup : StartupBase
+    {
+        public override void ConfigureServices(ITypeRegistrar registrar)
+        {
+            // register services here...
+            registrar.Register(typeof(IDisplayWriter), typeof(ConsoleDisplayWriter));
+        }
+
+        public override IConfigurator ConfigureCommands(IConfigurator config)
+        {
+            config.CaseSensitivity(CaseSensitivity.None);
+            config.SetApplicationName("LightInject.Cli");
+            config.ValidateExamples();
+
+            config.AddCommand<DefaultCommand>("default")
+                .WithDescription("Default command that displays some text.")
+                .WithExample(new[] { "default", "--verbose", "high" });
+
+            return config;
+        }
+    }
+}
