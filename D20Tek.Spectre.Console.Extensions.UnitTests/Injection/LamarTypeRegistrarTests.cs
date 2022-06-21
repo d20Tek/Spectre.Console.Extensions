3567772bf006109f861867aee9abc1c77d999f3e
Pedro Silva <pedrosi@microsoft.com>
2022-06-20 17:37:56 -0700

    Added LamarTypeRegistrar and LamarTypeResolver to integrate with DI system. Added unit tests for LamarTypeRegistrar and LamarTypeResolver classes.

---
+++ D20Tek.Spectre.Console.Extensions.UnitTests/Injection/LamarTypeRegistrarTests.cs
@@ -0,0 +1,84 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Spectre.Console.Extensions.Injection;
+using Lamar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace D20Tek.Spectre.Console.Extensions.UnitTests.Injection
+{
+    [TestClass]
+    public class LamarTypeRegistrarTests
+    {
+        public interface ITestService { };
+
+        public class TestService : ITestService { };
+
+        [TestMethod]
+        public void Register()
+        {
+            // arrange
+            var services = new ServiceRegistry();
+            var registrar = new LamarTypeRegistrar(services);
+
+            // act
+            registrar.Register(typeof(ITestService), typeof(TestService));
+
+            // assert
+            Assert.IsTrue(services.Any(x => x.ServiceType == typeof(ITestService)));
+            Assert.IsTrue(services.Any(x => x.ImplementationType == typeof(TestService)));
+        }
+
+        [TestMethod]
+        public void RegisterInstance()
+        {
+            // arrange
+            var services = new ServiceRegistry();
+            var registrar = new LamarTypeRegistrar(services);
+            var instance = new TestService();
+
+            // act
+            registrar.RegisterInstance(typeof(ITestService), instance);
+
+            // assert
+            Assert.IsTrue(services.Any(x => x.ServiceType == typeof(ITestService)));
+            Assert.IsTrue(services.Any(x => x.ImplementationInstance == instance));
+        }
+
+        [TestMethod]
+        public void RegisterLazy()
+        {
+            // arrange
+            var services = new ServiceRegistry();
+            var registrar = new LamarTypeRegistrar(services);
+
+            // act
+            registrar.RegisterLazy(typeof(ITestService), FactoryMethod);
+
+            // assert
+            Assert.IsTrue(services.Any(x => x.ServiceType == typeof(ITestService)));
+            Assert.IsNotNull(services[0].ImplementationFactory);
+        }
+
+        [TestMethod]
+        public void Build()
+        {
+            // arrange
+            var services = new ServiceRegistry();
+            var registrar = new LamarTypeRegistrar(services);
+
+            registrar.Register(typeof(ITestService), typeof(TestService));
+
+            // act
+            var resolver = registrar.Build();
+
+            // assert
+            Assert.IsNotNull(resolver);
+        }
+
+        [ExcludeFromCodeCoverage]
+        private ITestService FactoryMethod() => new TestService();
+    }
+}
---
+++ D20Tek.Spectre.Console.Extensions/Injection/LamarTypeResolver.cs
@@ -0,0 +1,53 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using Lamar;
+using Spectre.Console.Cli;
+
+namespace D20Tek.Spectre.Console.Extensions.Injection
+{
+    /// <summary>
+    /// Type resolver for Spectre.Console that uses the SimpleInjector framework.
+    /// </summary>
+    public sealed class LamarTypeResolver : ITypeResolver, IDisposable
+    {
+        private readonly Container _container;
+
+        /// <summary>
+        /// Constructor that takes a container for SimpleInjector.
+        /// </summary>
+        /// <param name="container">Container to use in type resolution.</param>
+        public LamarTypeResolver(Container container)
+        {
+            ArgumentNullException.ThrowIfNull(container, nameof(container));
+            _container = container;
+        }
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="type">The type to resolve.</param>
+        /// <returns>An instance of the specified type, or null if no registration for that
+        /// type exists.</returns>
+        public object? Resolve(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            return _container.GetInstance(type);
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            if (_container is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
