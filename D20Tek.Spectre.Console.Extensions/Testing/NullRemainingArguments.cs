4b070930f03942521aa92b8969a6b036ac9aa774
Pedro Silva <DarthPedro@gmail.com>
2025-06-05 15:03:04 -0700

    Made the NullRemainingArguments class public so it can be used in users' test projects.

--- D20Tek.Spectre.Console.Extensions/Testing/NullRemainingArguments.cs
+++ D20Tek.Spectre.Console.Extensions/Testing/NullRemainingArguments.cs
@@ -3,11 +3,17 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace D20Tek.Spectre.Console.Extensions.Testing;
 
-internal class NullRemainingArguments : IRemainingArguments
+/// <summary>
+/// Empty version of IRemainingArguments that is useful for unit testing commands.
+/// </summary>
+public class NullRemainingArguments : IRemainingArguments
 {
     private readonly List<string> _raw = [];
     private readonly ILookup<string, string?> _parsed;
 
+    /// <summary>
+    /// Default empty constructor.
+    /// </summary>
     public NullRemainingArguments()
     {
         _parsed = new List<KeyValuePair<string, string?>>().ToLookup(
@@ -15,9 +21,14 @@ internal class NullRemainingArguments : IRemainingArguments
             [ExcludeFromCodeCoverage] (kvp) => kvp.Value);
     }
 
+    /// <inheritdoc/>
     public ILookup<string, string?> Parsed => _parsed;
 
+    /// <inheritdoc/>
     public IReadOnlyList<string> Raw => _raw.AsReadOnly();
 
+    /// <summary>
+    /// Gets the default instance of NullRemainingArguments.
+    /// </summary>
     public static NullRemainingArguments Instance => new();
 }
