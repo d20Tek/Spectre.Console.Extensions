578362111545a1d46e3f94d2ba103be655025032
Pedro Silva <DarthPedro@gmail.com>
2025-06-06 15:03:40 -0700

    Added unit tests for TypeRegistrarExtensions WithLifetimes method.

---
+++ D20Tek.Spectre.Console.Extensions.UnitTests/Injection/TypeRegistrarExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using D20Tek.Spectre.Console.Extensions.Injection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Spectre.Console.Extensions.UnitTests.Injection;
+
+[TestClass]
+public class TypeRegistrarExtensionsTests
+{
+    [TestMethod]
+    public void WithLifetimes_WithDIContainer_ReturnsInterface()
+    {
+        // arrange
+        var registrar = new DependencyInjectionTypeRegistrar(new ServiceCollection());
+
+        // act
+        var result = registrar.WithLifetimes();
+
+        // assert
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType<ISupportLifetimes>(result);
+    }
+
+    [TestMethod]
+    public void WithLifetimes_WithLightInjectContainer_ThrowsException()
+    {
+        // arrange
+        var registrar = new LightInjectTypeRegistrar(new LightInject.ServiceContainer());
+
+        // act
+        Assert.Throws<InvalidOperationException>([ExcludeFromCodeCoverage]() => registrar.WithLifetimes());
+
+        // assert
+    }
+}
