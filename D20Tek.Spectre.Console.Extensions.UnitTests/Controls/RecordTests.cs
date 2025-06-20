839e057d5d75931cd83362bced87227fa55017ea
Pedro Silva <DarthPedro@gmail.com>
2025-06-20 11:58:25 -0700

    Added unit tests for records and StringExtensions.

---
+++ D20Tek.Spectre.Console.Extensions.UnitTests/Controls/RecordTests.cs
@@ -0,0 +1,58 @@
+﻿using D20Tek.Spectre.Console.Extensions.Controls.HistoryPrompt;
+using D20Tek.Spectre.Console.Extensions.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spectre.Console;
+using System.Text;
+
+namespace D20Tek.Spectre.Console.Extensions.UnitTests.Controls;
+
+[TestClass]
+public class RecordTests
+{
+
+    [TestMethod]
+    public void ReadLineRequest_WithChanges_ReturnUpdate()
+    {
+        // arrange
+        var request = new ReadLineRequest(null, null, false, null, [], []);
+
+        // act
+        var result = request with
+        {
+            AnsiConsole = new TestConsole(),
+            PromptStyle = new Style(Color.White),
+            IsSecret = true,
+            Mask = '$',
+            Items = [ "choice 1", "choice 2"],
+            History = [ "foo", "bar", "baz" ]
+        };
+
+        // assert
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    public void InputState_WithChanges_ReturnUpdate()
+    {
+        // arrange
+        var request = new ReadLineRequest(null, null, false, null, [], []);
+        var state = new InputState(null, null, -1, true, null, -1, null, false, false);
+
+        // act
+        var result = state with
+        {
+            Request = request,
+            Buffer = new StringBuilder(),
+            CursorIndex = 0,
+            CompletionItems = ["foo", "bar"],
+            HistoryIndex = 0,
+            Handled = true,
+            Done = true,
+            InsertMode = true,
+            SavedHistory = string.Empty
+        };
+
+        // assert
+        Assert.IsNotNull(result);
+    }
+}
---
+++ D20Tek.Spectre.Console.Extensions.UnitTests/Controls/StringExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using D20Tek.Spectre.Console.Extensions.Controls.HistoryPrompt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Spectre.Console.Extensions.UnitTests.Controls;
+
+[TestClass]
+public class StringExtensionsTests
+{
+
+    [TestMethod]
+    public void Repeat_WithTextAndCount_ReturnsString()
+    {
+        // arrange
+        var text = "x";
+
+        // act
+        var result = text.Repeat(5);
+
+        // assert
+        Assert.AreEqual("xxxxx", result);
+    }
+
+    [TestMethod]
+    public void Repeat_WithMultipleTextAndCount_ReturnsString()
+    {
+        // arrange
+        var text = "foo";
+
+        // act
+        var result = text.Repeat(3);
+
+        // assert
+        Assert.AreEqual("foofoofoo", result);
+    }
+
+    [TestMethod]
+    public void Repeat_WithTextAndSingleCount_ReturnsString()
+    {
+        // arrange
+        var text = "?";
+
+        // act
+        var result = text.Repeat(1);
+
+        // assert
+        Assert.AreEqual("?", result);
+    }
+
+    [TestMethod]
+    public void Repeat_WithEmptyTextAndCount_ReturnsEmptyString()
+    {
+        // arrange
+        var text = string.Empty;
+
+        // act
+        var result = text.Repeat(10);
+
+        // assert
+        Assert.AreEqual(string.Empty, result);
+    }
+
+    [TestMethod]
+    public void Repeat_WithNullText_ThrowsException()
+    {
+        // arrange
+        string text = null;
+
+        // act - assert
+        Assert.Throws<ArgumentNullException>([ExcludeFromCodeCoverage]() => text.Repeat(5));
+    }
+}
