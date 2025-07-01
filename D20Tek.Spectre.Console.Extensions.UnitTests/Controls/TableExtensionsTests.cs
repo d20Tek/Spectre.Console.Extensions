1d91102270cb5f889123652f6c209421a6eab9a4
Pedro Silva <DarthPedro@gmail.com>
2025-07-01 16:14:43 -0700

    Added TableExtensions.AddSeparatorRow method to the project. Added unit tests.

---
+++ D20Tek.Spectre.Console.Extensions.UnitTests/Controls/TableExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using D20Tek.Spectre.Console.Extensions.Controls;
+using D20Tek.Spectre.Console.Extensions.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spectre.Console;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace D20Tek.Spectre.Console.Extensions.UnitTests.Controls;
+
+[TestClass]
+public class TableExtensionsTests
+{
+
+    [TestMethod]
+    public void AddSeparatorRow_WithDefaultStyling_UpdatesTable()
+    {
+        // arrange
+        var console = new TestConsole();
+        int[] columns = [10, 10, 5];
+        var table = new Table().AddColumns(
+            new TableColumn("Col1").Centered().Width(10),
+            new TableColumn("Col2").Width(10),
+            new TableColumn("Col3").RightAligned().Width(5));
+
+        // act
+        table.AddSeparatorRow(columns);
+        console.Write(table);
+
+        // assert
+        Assert.AreEqual(1, table.Rows.Count);
+        Assert.AreEqual(3, table.Columns.Count);
+        StringAssert.Contains(console.Output, "──────────");
+    }
+
+    [TestMethod]
+    public void AddSeparatorRow_WithDifferentTableAndRowColumns_ThrowsException()
+    {
+        // arrange
+        int[] columns = [10, 10, 5];
+        var table = new Table();
+
+        // act - assert
+        Assert.Throws<InvalidOperationException>([ExcludeFromCodeCoverage]() => table.AddSeparatorRow(columns));
+    }
+
+    [TestMethod]
+    public void AddSeparatorRow_WithSpecifiedStyling_UpdatesTable()
+    {
+        // arrange
+        var console = new TestConsole();
+        int[] columns = [10, 10, 5];
+        var table = new Table().AddColumns(
+            new TableColumn("Col1").Centered().Width(10),
+            new TableColumn("Col2").Width(10),
+            new TableColumn("Col3").RightAligned().Width(5));
+
+        // act
+        table.AddSeparatorRow(columns, "white", '=');
+        console.Write(table);
+
+        // assert
+        Assert.AreEqual(1, table.Rows.Count);
+        Assert.AreEqual(3, table.Columns.Count);
+        StringAssert.Contains(console.Output, "==========");
+    }
+}
---
+++ D20Tek.Spectre.Console.Extensions/Controls/TableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Spectre.Console;
+
+namespace D20Tek.Spectre.Console.Extensions.Controls;
+
+/// <summary>
+/// Extension methods for Spectre.Console Table controls.
+/// </summary>
+public static class TableExtensions
+{
+    /// <summary>
+    /// Adds a separator row to the specified table. Allows the caller to also specify the 
+    /// separator character and its color. Defaults to '-' char and Color.Grey.
+    /// </summary>
+    /// <param name="table">Table to add row to.</param>
+    /// <param name="columnWidths">Array of column widths in this table.</param>
+    /// <param name="style">Color style to use for separator.</param>
+    /// <param name="separatorChar">Character to use for separator.</param>
+    public static void AddSeparatorRow(
+        this Table table,
+        int[] columnWidths,
+        string style = "grey",
+        char separatorChar = '─')
+    {
+        var row = Enumerable.Range(0, columnWidths.Length)
+                            .Select(i => new Markup($"[{style}]{new string(separatorChar, columnWidths[i])}[/]"))
+                            .ToArray();
+        table.AddRow(row);
+    }
+}
